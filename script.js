20179953641e0a375616d4333eb5163e1f81fc7a
Leo Bascunan <leo.elias.bascunan1@gmail.com>
2021-06-14 16:58:49 -0400

    fin de la segunda parte del proyecto (DOM)

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
+playerScore = 0;
+computerScore = 0;
+const buttons = document.querySelectorAll('button');
 
 function computerPlay() {
     let opciones = ["piedra", "papel", "tijera"];
@@ -7,30 +9,58 @@ function computerPlay() {
     return EleccionRandom;
 }
 
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
 
 function playRound(playerSelection) {
 
     let computerSelection = computerPlay();
+    let result = "";
 
     if ((playerSelection == 'piedra' && computerSelection == 'tijera') ||
         (playerSelection == 'tijera' && computerSelection == 'papel') ||
         (playerSelection == 'papel' && computerSelection == 'piedra')) {
-        alert("ganaste: " + playerSelection + " vence a " + computerSelection);
+
+        playerScore += 1;
+        result = ("ganaste: " + playerSelection + " vence a " + computerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+        if (playerScore == 5) {
+            result += '<br><br>Ganaste el juego! recarga la pagina para jugar denuevo'
+            disableButtons()
+        };
+                ;
     } else if (playerSelection == computerSelection) {
-        alert("empate");
+        result = ("empate. ambos eligieron " + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore);
+            
 
     } else if ((playerSelection == 'tijera' && computerSelection == 'piedra') ||
         (playerSelection == 'papel' && computerSelection == 'tijera') ||
         (playerSelection == 'piedra' && computerSelection == 'papel')) {
-        alert("perdiste: " + playerSelection + " pierde contra " + computerSelection);
+
+           computerScore += 1; 
+        result = ("perdiste: " + playerSelection + " pierde contra " + computerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore);
+        if (computerScore == 5) {
+            result += '<br><br>Perdiste el juego! recarga la pagina para jugar denuevo'
+            disableButtons()
+        };
+
     } else {
-        alert(playerSelection + " no es una opción válida");
-    } 
+        result = (playerSelection + " no es una opción válida");
+    }
     console.log("jugador eligió = " + playerSelection + " maquina eligió = " + computerSelection);
+
+
+    document.getElementById('result').innerHTML = result;
+
 }
 
 
-const buttons = document.querySelectorAll('button');
+
 buttons.forEach((button) => {
     button.addEventListener('click', function () { playRound(button.id) })
 
@@ -41,4 +71,9 @@ buttons.forEach((button) => {
 // console.log(playRound(playerSelection, computerSelection));
 
 
+//Add a div for displaying results and change all of your console.logs into DOM methods.
+//usando DOM   --  para eso usé getElementById y selecciono el id del div donde quiero que se vea
+// el resultado, se debe crear una variable vacia para que cada vez q se juege 1 ronda se vea el
+//resultado representado
 
+//Display the running score, and announce a winner of the game once one player reaches 5 points.
